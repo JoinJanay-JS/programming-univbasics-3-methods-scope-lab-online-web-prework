08f07488713507fa4c54958990da7d5577f637c2
Janay Bailey <tjanay87@gmail.com>
2019-08-19 19:33:52 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,3 +5,9 @@ end
 def toadstool
   puts status = "Thank You Mario! But Our Princess Is In Another Castle!"
 end
+def link
+  puts phrase = "It's Dangerous To Go Alone! Take This."
+end
+def any_phrase
+  puts phrase "Do A Barrel Roll!"
+end
